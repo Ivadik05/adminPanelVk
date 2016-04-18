aa8d767a41ce0f23b84f50d44babb93b9408de56
vadim.ch <ivadik05@gmail.com>
2016-04-18 13:35:28 +0300

    fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 USERNAME="root"
 HOSTS="vsevzaimosvyazano.ru"
 SCRIPT="cd ~/projects/vsevzaimosvyazano; npm run deploy"
-out="./log.txt"
+out="log.txt"
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} -A "${SCRIPT}" > ${out}
 done
