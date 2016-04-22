3c5c702b4be19ae68407f8ca3e7ef237f57ba5ff
ivadik05 <ivadik05@gmail.com>
2016-04-22 19:54:23 +0300

    add ga

--- src/app.tsx
+++ src/app.tsx
@@ -7,15 +7,27 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import { utils } from './utils';
 import store from './store';
-
+import * as ga from 'react-ga';
 const history = syncHistoryWithStore(browserHistory, store);
 const { pathname, search, hash } = window.location;
 const location = `${pathname}${search}${hash}`;
 
+
+if (process.env.NODE_ENV === 'production') {
+  ga.initialize('UA-76791325-1');
+}
+
+// 36942255
+
+let sendLocation = () => {
+  ga.pageview(window.location.pathname);
+};
+
+
 match(utils.tsReturnTypeFix({ routes, location }), () => {
   render(
       <Provider store={store}>
-        <Router routes={routes} history={history} />
+        <Router routes={routes} history={history} onUpdate={sendLocation} />
       </Provider>,
       document.getElementById('app')
   );
