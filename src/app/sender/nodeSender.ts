b47703574535c70f5a76b598fd6fe150db7e17fe
vadim.ch <ivadik05@gmail.com>
2016-04-25 06:17:11 +0300

    shit request

--- src/app/sender/nodeSender.ts
+++ src/app/sender/nodeSender.ts
@@ -44,7 +44,33 @@ export class NodeSender implements ISender {
     // TODO Promise.all с одним ответом
     // let requestName = request.getRequest().getName();
     let code = Execute.createPromiseCode(this.requestList);
-    this.io.send(new Execute(code), (response: BaseResponse<executeType>) => {
+    let customCode = `var categories = API.market.getAlbums({"owner_id":-61279456});
+                      var products = [];
+                      var preparateCategories = [];
+                      var i = -1;
+                      products = API.market.get({"owner_id":-61279456}).items;
+                      var productByAlbum;
+                      
+                      while (i < categories.items.length) {
+                        i = i + 1;
+                        if (categories.items[i].id > 0) {
+                          preparateCategories.push(categories.items[i]);
+                          productByAlbum = {"albumId": categories.items[i].id, "products": API.market.get({"owner_id":-61279456, "album_id": categories.items[i].id}).items};
+                        }
+                      };
+                      return[
+                        {
+                          "name": "market.album",
+                          "saver": market
+                          "data": {
+                            "categories": preparateCategories,
+                            "products": products,
+                            "productByAlbum": productByAlbum
+                          }
+                        },
+                        ${code}
+                      ];`;
+    this.io.send(new Execute(`return[${code}];`), (response: BaseResponse<executeType>) => {
       response.getData().map((res: BaseResponse<any>) => {
         this.store.dispatch({
           type: res.getSaverEvent(),
--- src/app/services/market/index.ts
+++ src/app/services/market/index.ts
@@ -6,7 +6,6 @@ import { events } from '../../../events';
 import { BaseResponse } from '../../../io/response/response';
 import { connector } from '../../../constants';
 import { IRequest } from '../../../io/interfaces';
-import { marketType } from '../../../io/types/index';
 
 class Market extends Service {
   private sender: WebSender = null;
@@ -18,7 +17,40 @@ class Market extends Service {
     // this.sender.send(new GetMarket('-61279456', '', true), (response: Array<marketType>) => {
     //   this.publishEvent(events.market.DRAW_MARKETS, response);
     // });
+    let requestList: Array<IRequest> = [
+      new GetMarket(connector.GROUP_ID, '', true),
+      new GetPage(connector.GROUP_ID, connector.PAGE_ABOUT),
+    ];
+    let code = Execute.createPromiseCode(requestList);
+    // {"name":"market.get","saver":"market","data":API.market.get({"owner_id":-61279456,"extended":1})}
+    let customCode = `var categories = API.market.getAlbums({"owner_id":-61279456});
+                      var products = [];
+                      var preparateCategories = [];
+                      var i = -1;
+                      products = API.market.get({"owner_id":-61279456}).items;
+                      var productByAlbum;
+                      
+                      while (i < categories.items.length) {
+                        i = i + 1;
+                        if (categories.items[i].id > 0) {
+                          preparateCategories.push(categories.items[i]);
+                          productByAlbum = {"albumId": categories.items[i].id, "products": API.market.get({"owner_id":-61279456, "album_id": categories.items[i].id}).items};
+                        }
+                      };
+                      return[
+                        {
+                          "name": "market.album",
+                          "saver": market
+                          "data": {
+                            "categories": preparateCategories,
+                            "products": products,
+                            "productByAlbum": productByAlbum
+                          }
+                        },
+                        ${code}
+                      ];`;
 
+    console.error(customCode);
   }
 
   private initListeners() {
